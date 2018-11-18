c0e1dad84d1393258a67b53d6b2439b3706ca5da
Keegan Street <kestreet@deloitte.com.au>
2018-11-18 14:38:18 +1100

    Initial commit

---
+++ 1-loaders/index.ts
@@ -0,0 +1,15 @@
+interface Person {
+	firstName: string;
+	lastName: string;
+}
+
+const greet = (person: Person) => {
+	return `Hello, ${person.firstName} ${person.lastName}`;
+};
+
+let user = {
+	firstName: 'Jane',
+	lastName: 'User'
+};
+
+document.body.innerHTML = greet(user);
